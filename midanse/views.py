d481279f7ae86901e64b17272b0bc31f0aec3351
--global <edison.bedoyag@udea.edu.co>
2020-09-26 21:20:13 -0500

    update

--- midanse/views.py
+++ midanse/views.py
@@ -8,19 +8,20 @@ from .serializers import *
 import polyglot
 from polyglot.text import Text, Word
 from polyglot.detect import Detector
+import json
 
 @api_view(['GET', 'POST'])
 def view_test(request):
     if request.method == 'GET':   
-        arabic_text = u"""
-أفاد مصدر امني في قيادة عمليات صلاح الدين في العراق بأن " القوات الامنية تتوقف لليوم
-الثالث على التوالي عن التقدم الى داخل مدينة تكريت بسبب
-انتشار قناصي التنظيم الذي يطلق على نفسه اسم "الدولة الاسلامية" والعبوات الناسفة
-والمنازل المفخخة والانتحاريين، فضلا عن ان القوات الامنية تنتظر وصول تعزيزات اضافية ".
-"""
-        detector = Detector(arabic_text)
+        text = request
+        detector = Detector(text)
         print(detector.language)
-        return Response({'data': 'hola del endpoint Get' , 'count': '1' })
+        return Response({'data': 'ji' , 'count': '1' })
     elif request.method == 'POST':
-        data=request.data
-        return Response(data, status=status.HTTP_201_CREATED)
+        print(request)
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        content = body['text']
+        detector = Detector(content)
+        print(detector.language.name)
+        return Response({'name':detector.language.name,'code':detector.language.code,'confidence':detector.language.confidence}, status=status.HTTP_201_CREATED)
